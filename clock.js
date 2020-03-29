277cc18e90cd44d71e6de70f8d558c68fc23929f
quasar529 <46216280+quasar529@users.noreply.github.com>
2020-03-29 18:38:05 +0900

    19.03.29_ToDoList&CSS

--- clock.js
+++ clock.js
@@ -28,7 +28,7 @@ function whatTime(){
     const seconds=date.getSeconds();
     clockTitle.innerText
     =`${hours<10 ? `0${hours}` : hours}:${
-        minutes<10 ? `$0{minutes}`: minutes}:${
+        minutes<10 ? `0${minutes}`: minutes}:${
             seconds < 10 ? `0${seconds}` : seconds
     }`;
 }
